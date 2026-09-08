3d81ec4d224d7bad17a5a8357b5f9d1f93cf0712
Shashwat Agrawal <129974594+ThisisShashwat@users.noreply.github.com>
2026-09-08 21:51:05 +0530

    YO FACE DETECTIONN

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import time
+
 import cv2
 import mediapipe as mp
-
+from mediapipe.tasks.python import BaseOptions
+from mediapipe.tasks.python.vision import FaceDetector, FaceDetectorOptions, RunningMode
 import streamlink
 
 TWITCH_CHANNEL ="plastuchino"
+
+FACECAM_X, FACECAM_Y = 0,0
+FACECAM_W, FACECAM_H = 400, 260
+
 
 def get_stream_url(channel):
     streams = streamlink.streams(f"https://twitch.tv/{channel}")
@@ -18,6 +25,13 @@ cap = cv2.VideoCapture(stream_url)
 if not cap.isOpened():
     raise RuntimeError("cant stream wtf")
 
+detector = FaceDetector.create_from_options(
+    FaceDetectorOptions(
+        base_options=BaseOptions(model_asset_path="face_detector.tflite"),
+        running_mode=RunningMode.VIDEO,
+    )
+)
+
 frame_count = 0
 
 while True:
@@ -26,10 +40,29 @@ while True:
         print("ughh?")
         break
     frame_count += 1
+
+    facecam = frame[FACECAM_Y:FACECAM_Y + FACECAM_H, FACECAM_X:FACECAM_X + FACECAM_W]
+
+    rgb = cv2.cvtColor(facecam, cv2.COLOR_BGR2RGB)
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb)
+
+    result = detector.detect_for_video(mp_image, int(time.time() *1000))
+    face_present = len(result.detections)  > 0
+
+    print(face_present)
+
+    for d in result.detections:
+
+        bb = d.bounding_box
+        cv2.rectangle(facecam, (bb.origin_x, bb.origin_y),
+                      (bb.origin_x + bb.width, bb.origin_y + bb.height), (0, 255, 0), 2)
+
+
     if frame_count % 30 == 0:
         print(f"frame {frame_count}, shape {frame.shape}")
 
-    cv2.imshow("stream", frame)
+    cv2.imshow("stream", facecam)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
